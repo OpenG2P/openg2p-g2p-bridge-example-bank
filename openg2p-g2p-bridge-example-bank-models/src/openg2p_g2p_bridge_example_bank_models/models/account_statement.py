d3ec21cf5f837d8a8b726353b33418a639b23f0b
tahzeer <tahzeer.ash@gmail.com>
2025-05-13 16:36:37 +0530

    Update example bank account statement models

--- openg2p-g2p-bridge-example-bank-models/src/openg2p_g2p_bridge_example_bank_models/models/account_statement.py
+++ openg2p-g2p-bridge-example-bank-models/src/openg2p_g2p_bridge_example_bank_models/models/account_statement.py
@@ -11,16 +11,23 @@ class DebitCreditTypes(Enum):
     DEBIT = "debit"
     CREDIT = "credit"
 
+class AccountStatementStatus(Enum):
+    NOT_APPLICABLE = "NOT_APPLICABLE"
+    PENDING = "PENDING"
+    PROCESSING = "PROCESSING"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
 
 class AccountStatement(BaseORMModelWithTimes):
     __tablename__ = "account_statements"
     account_number: Mapped[str] = mapped_column(String, index=True)
     account_statement_lob: Mapped[str] = mapped_column(Text, nullable=True)
+    account_statement_generation_status: Mapped[AccountStatementStatus] = mapped_column(
+        SqlEnum(AccountStatementStatus), default=AccountStatementStatus.PENDING
+    )
     account_statement_date: Mapped[datetime.date] = mapped_column(
         DateTime, default=datetime.date(datetime.utcnow())
     )
-    # TODO: add status enum
-
 
 class AccountingLog(BaseORMModelWithTimes):
     __tablename__ = "accounting_logs"
